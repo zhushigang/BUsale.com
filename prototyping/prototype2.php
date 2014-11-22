6cdeb33e99d28cda046650fa01167c522be07185
zhushigang <zhushigang@gmail.com>
2014-11-21 21:06:21 -0500

    Ready for tests
    Updated index.html and prototype2.php so that we can test our website

--- prototyping/prototype2.php
+++ prototyping/prototype2.php
@@ -3,8 +3,10 @@ include 'Database.php';
 $db123 = new Database();
 $images=$db123->fetchAll("Prototype1");
 foreach($images as $image){
-	echo '<img src="'.$image[url].'" />';
-	echo '<p>'.$image[user].'</p>';
-	echo '<p>'.$image[caption].'</p>';
+	if(strpos($image[caption],$_POST['search'])!==false){
+		echo '<img src="'.$image[url].'" />';
+		echo '<p>'.$image[user].'</p>';
+		echo '<p>'.$image[caption].'</p>';
+	}
 }
 ?>
